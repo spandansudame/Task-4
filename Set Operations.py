28d684992bd1c32fb43ffc4d2e52b10a01bdfa4b
spandansudame <145888981+spandansudame@users.noreply.github.com>
2023-09-24 15:40:32 +0530

    Add files via upload

---
+++ Set Operations.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 24 15:30:45 2023
+
+@author: Spandan
+"""
+# Program to perform different set operations like in mathematics
+
+# define three sets
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+
+# set union
+print("Union of E and N is",E|N)
+
+# set intersection
+print("Intersection of E and N is",E&N)
+
+# set difference
+print("Difference of E and N is",E-N)
+
+# set symmetric difference
+print("Symmetric difference of E and N is",E^N)
