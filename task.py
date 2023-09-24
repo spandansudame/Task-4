5d1bf25239a78dd8e44cd4f0097e00b465e3a733
spandansudame <145888981+spandansudame@users.noreply.github.com>
2023-09-24 11:15:29 +0530

    Add files via upload
    Python program for Fibonacci Series

---
+++ task.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 24 11:05:54 2023
+
+@author: Spandan
+"""
+
+n=10
+num1=0
+num2=1
+next_number=num2 
+count=1
+ 
+while count<=n:
+    print(next_number, end=" ")
+    count+=1
+    num1, num2=num2, next_number
+    next_number=num1 + num2
+    print()
